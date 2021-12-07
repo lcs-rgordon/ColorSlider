e9361230867d9e65e817344d00481548acca695f
Russell Gordon <rgordon@lcs.on.ca>
2021-12-07 12:34:56 -0500

    Initial Commit

---
+++ ColorSlider/ColorSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderApp.swift
+//  ColorSlider
+//
+//  Created by Russell Gordon on 2021-12-07.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
